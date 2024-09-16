216e00d36b938e1bc31e12044bd8a582c949c01e
sidsag1 <sagarbarpanda1@gmail.com>
2024-09-16 21:29:48 +0530

    Add plan on how to build a RAG application with Ollama

---
+++ app.py
@@ -0,0 +1,5 @@
+# Need document loader to load the document from website (langchain_community?). Check chunking down webpage. 
+# Need ollama to run the LLM model (llama3.1?)
+# Need visual representation (streamlit?)
+# First retrieve from document (if present), else retrieve information from LLM model?
+# 
